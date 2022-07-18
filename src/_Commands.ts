df0e7db78c5cd9d8e07b290f4cc83e807285416c
Juanan <juan-antonio.sanchez@necomplus.com>
2022-07-18 07:13:36 +0200

    🎨 Import command files

--- src/_Commands.ts
+++ src/_Commands.ts
@@ -1,5 +1,22 @@
 import { commands as rolls } from "./commands/dice";
-import { commands as plays } from "./commands/play";
+import { commands as play } from "./commands/play";
+import { commands as volume } from "./commands/volume";
+import { commands as resume } from "./commands/resume";
+import { commands as stop } from "./commands/stop";
+import { commands as pause } from "./commands/pause";
+import { commands as skip } from "./commands/skip";
+import { commands as queue } from "./commands/queue";
+import { commands as repeat } from "./commands/repeat";
 import { Command } from "./types/ICommand";
 
-export const CommandList: Command[] = [...rolls, ...plays];
+export const CommandList: Command[] = [
+  ...rolls,
+  ...play,
+  ...volume,
+  ...resume,
+  ...stop,
+  ...pause,
+  ...skip,
+  ...queue,
+  ...repeat,
+];
