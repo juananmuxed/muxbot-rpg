884d92c75fbd91bbb3313fb5ff311c3edc557222
Juanan <juan-antonio.sanchez@necomplus.com>
2022-07-18 07:14:14 +0200

    🏷️ Update client types discordjs

--- src/types/common/discord.d.ts
+++ src/types/common/discord.d.ts
@@ -1,9 +1,11 @@
 import { Collection } from "discord.js";
 import DisTube from "distube";
+import { LanguageModule } from "../ILanguage";
 
 declare module "discord.js" {
   export interface Client {
     commands: Collection;
     disTube: DisTube;
+    lang: string;
   }
 }
