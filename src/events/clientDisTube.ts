b10c62e7460363bb067991b8b3609367aeb2ecb9
Juanan <juan-antonio.sanchez@necomplus.com>
2022-07-18 07:19:59 +0200

    🚧 Add events to Distube and change strings

--- src/events/clientDisTube.ts
+++ src/events/clientDisTube.ts
@@ -1,6 +1,12 @@
-import { MessageEmbed } from "discord.js";
-import { DisTube, Queue, Song } from "distube";
+import {
+  Constants,
+  GuildTextBasedChannel,
+  Message,
+  MessageEmbed,
+} from "discord.js";
+import { DisTube, Playlist, Queue, Song } from "distube";
 import { client } from "..";
+import { _ } from "../utils/utils";
 
 export const clientDisTube = () => {
   client.disTube = new DisTube(client, {
@@ -10,28 +16,126 @@ export const clientDisTube = () => {
     youtubeDL: false,
   });
 
+  const clearPresence = (): void => {
+    client.user?.setPresence({
+      activities: [
+        {
+          name: "🤖",
+          type: Constants.ActivityTypes.WATCHING,
+        },
+      ],
+    });
+  };
+
   const status = (queue: Queue) =>
-    `Volume: \`${queue.volume}%\` | Filter: \`${
-      queue.filters.join(", ") || "Off"
-    }\` | Loop: \`${
+    `${_("Volume", client.lang)}: \`${queue.volume}%\` | ${_(
+      "Loop",
+      client.lang
+    )}: \`${
       queue.repeatMode
         ? queue.repeatMode === 2
-          ? "All Queue"
-          : "This Song"
-        : "Off"
-    }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
+          ? _("All queue", client.lang)
+          : _("This song", client.lang)
+        : _("Off", client.lang)
+    }\``;
 
-  client.disTube.on("playSong", (queue: Queue, song: Song) =>
-    queue.textChannel?.send({
-      embeds: [
-        new MessageEmbed()
-          .setColor("GREEN")
-          .setDescription(
-            `🎵 | Playing \`${song.name}\` - \`${
-              song.formattedDuration
-            }\`\nRequested by: ${song.user}\n${status(queue)}`
-          ),
-      ],
+  client.disTube
+    .on("playSong", (queue: Queue, song: Song) => {
+      client.user?.setPresence({
+        activities: [
+          {
+            name: song.name,
+            type: Constants.ActivityTypes.LISTENING,
+            url: song.url,
+          },
+        ],
+      });
+
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setImage(song.thumbnail || "")
+        .addField(
+          `🎵 | ${_("Playing", client.lang)}`,
+          song.name || _("No music data", client.lang)
+        )
+        .addField(_("Options", client.lang), status(queue))
+        .addField(
+          _("Requested by", client.lang),
+          `<@${song.user?.id}>`
+        );
+
+      queue.textChannel?.send({
+        embeds: [embed],
+      });
     })
-  );
+    .on("addSong", (queue: Queue, song: Song) => {
+      const embed = new MessageEmbed()
+        .setColor("GREEN")
+        .addField(
+          `📢 | ${_("Added song", client.lang)}`,
+          song.name || _("No music data", client.lang)
+        );
+      queue.textChannel?.send({
+        embeds: [embed],
+      });
+    })
+    .on("addList", (queue: Queue, playlist: Playlist) => {
+      const embed = new MessageEmbed()
+        .setColor("GREEN")
+        .addField(
+          `📢 | ${_("Added playlist", client.lang)}`,
+          `${playlist.name || _("No music data", client.lang)}: ${
+            playlist.songs.length
+          } ${
+            playlist.songs.length > 1
+              ? _("songs", client.lang)
+              : _("song", client.lang)
+          }`
+        );
+      queue.textChannel?.send({
+        embeds: [embed],
+      });
+    })
+    .on("error", (channel: GuildTextBasedChannel, e: Error) => {
+      channel.send(
+        `🚨 | ${_("Alert", client.lang)}: ${e
+          .toString()
+          .slice(0, 1974)}`
+      );
+      clearPresence();
+      console.error(e);
+    })
+    .on("empty", (queue: Queue) => {
+      const embed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(
+          `🔴 | ${_(
+            "Empty voice channel, leaving channel...",
+            client.lang
+          )}`
+        );
+      clearPresence();
+      queue.textChannel?.send({
+        embeds: [embed],
+      });
+    })
+    .on("searchNoResult", (message: Message) => {
+      const embed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`🔴 | ${_("No results found", client.lang)}`);
+      message.channel?.send({
+        embeds: [embed],
+      });
+    })
+    .on("finish", (queue: Queue) => {
+      const embed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(
+          `🔴 | ${_("Queue finished, leaving channel", client.lang)}`
+        );
+      clearPresence();
+      queue.textChannel?.send({
+        embeds: [embed],
+      });
+    });
 };
