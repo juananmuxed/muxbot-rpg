353ba10360f1a82c5e754093bd0e92c5c7cb4461
Juanan <juan-antonio.sanchez@necomplus.com>
2022-07-18 07:18:01 +0200

    🚧 Add pause command

---
+++ src/commands/pause.ts
@@ -0,0 +1,54 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { Client, CommandInteraction, GuildMember } from "discord.js";
+import { Command, getCommands } from "../types/ICommand";
+import { _ } from "../utils/utils";
+
+class Constants {
+  static readonly COMMAND_NAME = "pause";
+}
+
+const getSlashCommand = (
+  name: string,
+  lang: string = "en"
+): Omit<
+  SlashCommandBuilder,
+  "addSubcommand" | "addSubcommandGroup"
+> => {
+  return new SlashCommandBuilder()
+    .setName(_(name, lang))
+    .setDescription(`⏸️ ${_("Pause music", lang)}`);
+};
+
+const getInteraction =
+  (lang: string = "en") =>
+  async (interaction: CommandInteraction, client: Client) => {
+    const { member } = interaction;
+    const voiceChannel = (member as GuildMember)?.voice.channel;
+
+    if (!voiceChannel)
+      return interaction.reply({
+        content: `⛔ ${_("You must be in a voice channel", lang)}`,
+        ephemeral: true,
+      });
+
+    const queue = client.disTube.getQueue(voiceChannel);
+    if (!queue)
+      return interaction.reply({
+        content: `⛔ ${_("There is no queue", lang)}`,
+      });
+
+    client.disTube.pause(voiceChannel);
+
+    interaction.reply({
+      ephemeral: true,
+      content: `⏸️ ${_("Music has been paused", lang)}`,
+    });
+  };
+
+const commands: Command[] = getCommands(
+  Constants.COMMAND_NAME,
+  getSlashCommand,
+  getInteraction
+);
+
+export { commands };
